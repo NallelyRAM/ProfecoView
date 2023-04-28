6d8d8f5e2e8a5ca34257c7ed8f8fd01ffc5dc641
LDoming <luis.dominguez218540@potros.itson.edu.mx>
2023-04-27 23:14:01 -0700

    CORREGI LO DE LA NALLE :D

--- js/script.js
+++ js/script.js
@@ -6,9 +6,8 @@ function login(){
 
     if(user=="nalleprueba" && password=="123456"){
         //console.log("Haz inicado sesión!!")
-        alert("Haz iniciado sesión!!"
+        alert("Haz iniciado sesión!!")
     }else{
         alert("Datos incorrectos")
-
     }
 }
