21ffdda162b4e7ccc1782ee3a0037f5fad6afedb
NallelyRAM <nallely.ramirez216724@potros.itson.edu.mx>
2023-05-12 23:58:46 -0700

    Agregar oferta js

---
+++ js/agregarProducto.js
@@ -0,0 +1,67 @@
+const url = 'https://jsonplaceholder.typicode.com/users';
+const respuesta = document.querySelector("#respuesta")
+const btnAgregar = document.querySelector("#botonActualizar")
+
+document.addEventListener("DOMContentLoaded", mostrarUsuarios)
+
+btnAgregar.addEventListener("click", agregarUsuario)
+
+async function mostrarUsuarios() {
+    const respuesta = await fetch(url)
+    const usuarios = await respuesta.json()
+    mostrarListaUsuarios(usuarios)
+}
+
+async function agregarUsuario() {
+    const name = document.querySelector("#nombreProducto").value
+    const username = document.querySelector("#marcaProducto").value
+    const lat = document.querySelector("#precioProducto").value
+    const lng = document.querySelector("#stockProducto").value
+
+    const nuevoUsuario = {
+        name: name,
+        username: username,
+        address: {
+            geo: {
+                lat: lat,
+                lng: lng
+            }
+        }
+    }
+
+    const respuesta = await fetch(url, {
+        method: 'POST',
+        body: JSON.stringify(nuevoUsuario),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    const usuarioCreado = await respuesta.json()
+    mostrarUsuario(usuarioCreado)
+    console.log(respuesta)
+}
+
+
+function mostrarListaUsuarios(usuarios) {
+    usuarios.forEach(usuario => {
+        const fila = document.createElement('tr')
+        fila.innerHTML = `
+            <td>${usuario.name}</td>
+            <td>${usuario.username}</td>
+            <td>${usuario.address.geo.lat}</td>
+            <td>${usuario.address.geo.lng}</td>
+        `
+        respuesta.appendChild(fila)
+    })
+}
+
+function mostrarUsuario(usuario) {
+    const fila = document.createElement('tr')
+    fila.innerHTML = `
+        <td>${usuario.name}</td>
+        <td>${usuario.username}</td>
+        <td>${usuario.address.geo.lat}</td>
+        <td>${usuario.address.geo.lng}</td>
+    `
+    respuesta.appendChild(fila)
+}
