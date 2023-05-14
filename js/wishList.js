7a063abd9dbe104a22f3ba6ce67aa19d3dcfec53
Joaquín Castro <116616837+imJoaquin@users.noreply.github.com>
2023-05-13 20:10:15 -0700

    btnEditar

--- js/wishList.js
+++ js/wishList.js
@@ -9,20 +9,14 @@ selectedComments.forEach(comment => {
   row.appendChild(document.createElement("td")).textContent = comment.id;
   row.appendChild(document.createElement("td")).textContent = comment.email;
 
-  const btnEdit = document.createElement("button");
-  btnEdit.textContent = "Editar";
-  btnEdit.classList.add("btn");
-  btnEdit.classList.add("btn-primary");
+//Boton eliminar
   const btnDelete = document.createElement("button");
   btnDelete.textContent = "Eliminar";
   btnDelete.classList.add("btn");
   btnDelete.classList.add("btn-danger")
-  const tdEdit = document.createElement("td");
   const tdDelete = document.createElement("td");
-  tdEdit.appendChild(btnEdit);
   tdDelete.appendChild(btnDelete);
-  row.appendChild(tdEdit);
   row.appendChild(tdDelete);
-
   selectedCommentsTableBody.appendChild(row);
+
 });
