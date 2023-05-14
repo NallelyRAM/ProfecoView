23859114c460c92ed729206924136fb0ccf9f3e6
NallelyRAM <nallely.ramirez216724@potros.itson.edu.mx>
2023-05-13 19:59:35 -0700

    boton editar y eliminar en la wish

--- js/wishList.js
+++ js/wishList.js
@@ -8,5 +8,17 @@ selectedComments.forEach(comment => {
   row.appendChild(document.createElement("td")).textContent = comment.name;
   row.appendChild(document.createElement("td")).textContent = comment.id;
   row.appendChild(document.createElement("td")).textContent = comment.email;
+
+  const btnEdit = document.createElement("button");
+  btnEdit.textContent = "Editar";
+  const btnDelete = document.createElement("button");
+  btnDelete.textContent = "Eliminar";
+  const tdEdit = document.createElement("td");
+  const tdDelete = document.createElement("td");
+  tdEdit.appendChild(btnEdit);
+  tdDelete.appendChild(btnDelete);
+  row.appendChild(tdEdit);
+  row.appendChild(tdDelete);
+
   selectedCommentsTableBody.appendChild(row);
 });
