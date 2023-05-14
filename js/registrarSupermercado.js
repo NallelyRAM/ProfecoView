fb090878144ad7d28f828e6418d31b3c5050d2fe
Joaquín Castro <116616837+imJoaquin@users.noreply.github.com>
2023-05-13 19:11:56 -0700

    Registro Supermercado

---
+++ js/registrarSupermercado.js
@@ -0,0 +1,52 @@
+const form = document.getElementById('myFormrg');
+
+form.addEventListener('submit', (event) => {
+  // Evita que el formulario se envíe automáticamente
+event.preventDefault();
+
+  // Obtener los valores de los campos
+const nombre = document.getElementById('nombre').value;
+const apellido = document.getElementById('apellido').value;
+const email = document.getElementById('email').value;
+const pwd = document.getElementById('pwd').value;
+const cpwd = document.getElementById('cpwd').value;
+
+  // Verificar que todos los campos tengan un valor
+if (!nombre || !apellido || !email || !pwd || !cpwd) {
+    alert('Por favor complete todos los campos.');
+    return;
+}
+
+  // Verificar que la dirección de correo electrónico sea válida
+const emailRegex = /^\S+@\S+\.\S+$/;
+if (!emailRegex.test(email)) {
+    alert('Por favor ingrese una dirección de correo electrónico válida.');
+    return;
+}
+
+  // Verificar que la contraseña y la confirmación sean iguales
+if (pwd !== cpwd) {
+    alert('La contraseña y la confirmación no coinciden.');
+    return;
+}
+
+  // Guardar los datos del usuario en el localStorage
+const usuario = {
+    nombre,
+    apellido,
+    email,
+    pwd
+};
+
+localStorage.setItem('usuario', JSON.stringify(usuario));
+  //console.log(usuario)
+const usuarioActivo = JSON.parse(localStorage.getItem('usuario'));
+  //console.log(usuarioActivo)
+const nombreUsuario = usuarioActivo.nombre;
+  //console.log(nombreUsuario)
+if(nombreUsuario != null){
+    alert("Bienvenido "+nombreUsuario);
+}
+  // Redirigir al usuario a la siguiente página
+window.location.href = '../html/supermercado.html';
+});
