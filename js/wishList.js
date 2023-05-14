db8e417ae6f9566211dcda0291296e493e0a1232
Joaquín Castro <116616837+imJoaquin@users.noreply.github.com>
2023-05-14 01:43:58 -0700

    Correcciones de referencias - Mapping WishList comentariado

--- js/wishList.js
+++ js/wishList.js
@@ -1,5 +1,4 @@
-
-
+const url = 'https://jsonplaceholder.typicode.com/users';
 const selectedComments = JSON.parse(localStorage.getItem("selectedComments"));
 const selectedCommentsTableBody = document.querySelector("#selected-comments-table tbody");
 
@@ -8,6 +7,41 @@ selectedComments.forEach(comment => {
   row.appendChild(document.createElement("td")).textContent = comment.name;
   row.appendChild(document.createElement("td")).textContent = comment.id;
   row.appendChild(document.createElement("td")).textContent = comment.email;
+
+/*
+  //Mapping
+  const wishList = {
+    //"id:" Math.floor(Math.random() * 1000),
+    "deseo": deseo
+    //"supermercado":{
+    //"id": 
+        //idSupermercado
+      //},
+    //"consumidor":{
+      //"id": 
+        //idConsumidor
+      //}
+  }
+
+    let idProductoAgregado = 0
+    fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(wishList)
+    })
+      .then(response => response.json())
+      .then(data => {
+        idProductoAgregado = data.id
+        console.log(data);
+      })
+      .catch(error => {
+        // Manejar errores
+        console.error('Error:', error);
+        return
+      });
+*/
 
 //Boton eliminar
   const btnDelete = document.createElement("button");
@@ -21,7 +55,7 @@ selectedComments.forEach(comment => {
 
 
 //Accion para que elimine un producto de la wish
- btnDelete.addEventListener("click", () => {
+  btnDelete.addEventListener("click", () => {
     if (confirm("¿Está seguro que desea eliminar este comentario?")) {
       // Eliminar fila de la tabla
       row.remove();
